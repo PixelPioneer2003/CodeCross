2b33c2976136db99b2503365369fe066493e8b6f
PixelPioneer2003 <154107788+PixelPioneer2003@users.noreply.github.com>
2025-09-05 02:57:48 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: 'https://codebet-backend.onrender.com/api', // or your backend URL
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://codecross-backend.onrender.com/api",
 });
 
 export default instance;
